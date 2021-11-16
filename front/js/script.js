1de064498da4f0c4924e47e24b2319928c08ed20
pinto <da.pinto75@gmail.com>
2021-11-16 23:48:54 +0100

    affichage des produits page acceuil

---
+++ front/js/script.js
@@ -0,0 +1,24 @@
+
+const productToHtml = (p) => {
+    return `
+    <a href="./product.html?id=${p._id}">
+        <article>
+            <img src="${p.imageUrl}" alt="${p.altTxt}">
+            <h3 class="productName">${p.name}</h3>
+            <p class="productDescription">${p.description}</p>
+        </article>
+    </a>
+    `;  
+};
+
+  const products = fetch('http://127.0.0.1:3000/api/products')
+  .then((response) => {
+       return response.json() 
+    })
+  .then((content) => { 
+    const finalHtml = content.map(productToHtml);
+    document.getElementById('items').innerHTML = finalHtml.join('');
+   })
+   .catch((err) => {
+    document.getElementById('items').innerHTML = `<h4>Errror lors du fetch des produits</h4>`;
+   })
